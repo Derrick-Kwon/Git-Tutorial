9336d0a269d29c4ab79988d9fdf0f895f7fcf8b4
Derrick Kwon <f5863@naver.com>
2021-11-11 15:41:58 +0900

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # import tensorflow.keras as keras
 #git commit -m "first commit" #과제에 히스토리(순서)를 표시해주는것!
-#
+#$ git remote remove origin 그 주소 바꾸기
+#$ git push -u origin main #이런식으로 코드를 보낼 수 있다!
+
 print("git hub 시작했다!!! ^^")
+print("update 해볼까?")
